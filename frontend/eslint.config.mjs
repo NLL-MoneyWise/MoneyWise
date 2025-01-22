d0f497595c9e70a11a4b50165b6d62bdffb9bc6c
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-01-22 22:59:23 +0900

    chore : 프리티어 eslint 설정

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,16 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            '@typescript-eslint/no-unused-vars': 'error',
+            'prettier/prettier': ['error', {}, { usePrettierrc: true }]
+        },
+        extends: [
+            'plugin:@typescript-eslint/recommended',
+            'plugin:prettier/recommended'
+        ]
+    }
 ];
 
 export default eslintConfig;
