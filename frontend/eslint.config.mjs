e6268937dae1722d6761b2d9fb63e972b48a9c49
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-01-22 23:27:54 +0900

    개발환경 설정  (#17)
    * env : jest, chormatic 의존성 추가
    
    * chore : storybook 스닛펫 추가
    
    * ci : storybook ci 파일 추가
    
    * chore : 프리티어 eslint 설정
    
    * chore : husky 세팅
    
    * style : 포맷팅
    
    * chore : 허스키 명령어 수정
    
    * chore : husky 세팅

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,16 +1,35 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettier from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        ignores: [
+            'src/stories',
+            'src/**/*.stories.{js,ts,tsx}',
+            '**/stories/**/*'
+        ]
+    },
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        files: ['**/*.{js,ts,tsx}'],
+        plugins: {
+            prettier: prettier
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            '@typescript-eslint/no-unused-vars': 'error',
+            'prettier/prettier': 'error'
+        }
+    }
 ];
 
 export default eslintConfig;
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    // stories 파일 빌드 제외
+    config.module.rules.push({
+      test: /(\.stories\.tsx?$|stories\/)/,
+      loader: "ignore-loader",
+    });
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
