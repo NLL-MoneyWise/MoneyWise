f79afb2c6d373d4059b61365a026fd91cb4e4571
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-01-22 23:26:03 +0900

    chore : husky 세팅

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import prettier from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,17 +11,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+    {
+        ignores: [
+            'src/stories',
+            'src/**/*.stories.{js,ts,tsx}',
+            '**/stories/**/*'
+        ]
+    },
     ...compat.extends('next/core-web-vitals', 'next/typescript'),
     {
+        files: ['**/*.{js,ts,tsx}'],
+        plugins: {
+            prettier: prettier
+        },
         rules: {
             'react/react-in-jsx-scope': 'off',
             '@typescript-eslint/no-unused-vars': 'error',
-            'prettier/prettier': ['error', {}, { usePrettierrc: true }]
-        },
-        extends: [
-            'plugin:@typescript-eslint/recommended',
-            'plugin:prettier/recommended'
-        ]
+            'prettier/prettier': 'error'
+        }
     }
 ];
 
