51558219785856107497947b0dcd4752bb6c2280
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-02-07 15:33:51 +0900

    design : 폰트 적용

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,15 @@
+// app/layout.tsx
 import ToastContainer from './components/Common/Toast/ToastContainer';
 import './globals.css';
+
+import { Noto_Sans_KR } from 'next/font/google';
+
+const notoSansKr = Noto_Sans_KR({
+    subsets: ['latin'],
+    weight: ['400', '500', '700'],
+    display: 'swap',
+    preload: true
+});
 
 export default function RootLayout({
     children
@@ -7,9 +17,24 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body>
-                {children}
+        <html>
+            <body lang="ko" className={notoSansKr.className}>
+                <main
+                    className="min-w-[375px] 
+                             max-w-[475px] 
+                             w-full 
+                             h-screen
+                             min-h-screen
+                             bg-white
+                             flex
+                             flex-col
+                             mx-auto
+                             box-border
+                             border-x
+                             border-gray-200"
+                >
+                    {children}
+                </main>
                 <div id="toast-portal"></div>
                 <div id="modal-portal"></div>
                 <ToastContainer />
