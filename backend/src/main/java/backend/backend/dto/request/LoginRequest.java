758bbabff5feb6a647c496850cb5efebe00c8edb
jionu102 <jionu102@naver.com>
2025-02-09 22:54:20 +0900

    test: AuthService테스트 추가 - AuthServiceTest추가 및 Builder와 new 연산자 사용으로 인해 기본생성자 및 파라미터 생성자 생성

--- backend/src/main/java/backend/backend/dto/request/LoginRequest.java
+++ backend/src/main/java/backend/backend/dto/request/LoginRequest.java
@@ -3,11 +3,13 @@ package backend.backend.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "이메일 입력은 필수입니다.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
---
+++ backend/src/test/java/backend/backend/service/AuthServiceTest.java
@@ -0,0 +1,28 @@
+package backend.backend.service;
+
+import backend.backend.dto.request.LoginRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthServiceTest {
+    @Autowired
+    private AuthService authService;
+    @Test
+    @DisplayName("실제 로그인 테스트")
+    void loginSuccess() {
+        String email = "test@naver.com";
+        String password = "testPassword@1231";
+
+        LoginRequest request = LoginRequest.builder().email(email).password(password).build();
+
+        String accessToken = authService.login(request);
+
+        System.out.println(accessToken);
+
+        Assertions.assertNotNull(accessToken);
+    }
+}
