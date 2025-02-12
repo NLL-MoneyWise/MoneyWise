0cd1c7624466b0088361b17cacb078a0062d0176
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-02-13 01:56:24 +0900

    fix : 타입에러 수정

--- frontend/src/app/common/components/Button/Button.tsx
+++ frontend/src/app/common/components/Button/Button.tsx
@@ -3,23 +3,32 @@ import React from 'react';
 import { clsx } from 'clsx';
 import { ReactNode } from 'react';
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
-    width: number;
-    height: number;
+    width?: string | number;
+    height?: string | number;
     handleClick?: () => void;
 }
 
+// 차후에 수정
 const Button = ({
     children,
     handleClick,
+    width = '100%',
+    height = '45px',
+    className,
     ...rest
-}: ButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+}: ButtonProps) => {
     return (
         <button
             className={clsx(
-                'bg-primary text-white flex justify-center items-center py-[10px] rounded-xl text-sm w-full h-[45px]'
+                'bg-primary text-white flex justify-center items-center py-[10px] rounded-xl text-sm',
+                className
             )}
+            style={{
+                width: typeof width === 'number' ? `${width}px` : width,
+                height: typeof height === 'number' ? `${height}px` : height
+            }}
             onClick={handleClick}
             {...rest}
         >
