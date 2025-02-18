442afabb08653e984382e10dc1ca108d506dd1f8
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-02-18 14:19:08 +0900

    feat : 구현된 기능을 바탕으로 로그인 기능 보완
    - 토큰은 쿠키에 저장하도록 설정
    - 유저 정보는 로컬 스토리지에 저장하도록 설정

--- frontend/src/app/auth/hooks/useAuthMutation.ts
+++ frontend/src/app/auth/hooks/useAuthMutation.ts
@@ -1,18 +1,32 @@
+import { LoginResponse } from './../types/reponse/response-login';
 import { useToastStore } from '@/app/common/hooks/useToastStore';
 import { AuthRepositoryimpl } from '../respository';
 import { useMutation } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+import saveToekn from '../util/saveToekn';
+import { useUserStore } from '@/stores/userStore';
 
 const useAuthMutation = () => {
     const { addToast } = useToastStore();
     const authRepository = new AuthRepositoryimpl();
     const router = useRouter();
+    const searchParams = useSearchParams();
+    const { setUser } = useUserStore();
 
     const loginMutation = useMutation({
         mutationFn: authRepository.login.bind(authRepository),
-        onSuccess: async () => {
-            addToast('로그인에 성공했습니다.', 'success');
-            router.push('/');
+        onSuccess: async (response: LoginResponse) => {
+            const { message, accessToken, refreshToken, email, nickName } =
+                response;
+            // 토큰 저장
+            await saveToekn(accessToken, refreshToken);
+            // 유저 정보 로컬 저장
+            setUser({ email, nickName });
+            // 갈려하던 파라미터 저장
+
+            const callbackUrl = searchParams.get('callbackUrl') || '/';
+            addToast(message, 'success');
+            router.push(callbackUrl);
         }
     });
 
