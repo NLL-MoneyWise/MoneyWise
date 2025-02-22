0bd95c28e1ce9fca5a41f53b928cbef009248e7a
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-02-22 14:40:51 +0900

    fix : 로그인 page 페이지 수정
    - useSearchparmas는 suspense를 필요로 하기 URLSearchParams를 불러와 성공 이후에 가져오도록 처리

--- frontend/src/app/auth/hooks/useAuthMutation.ts
+++ frontend/src/app/auth/hooks/useAuthMutation.ts
@@ -1,8 +1,9 @@
+'use client';
 import { LoginResponse } from './../types/reponse/response-login';
 import { useToastStore } from '@/app/common/hooks/useToastStore';
 import { AuthRepositoryimpl } from '../respository';
 import { useMutation } from '@tanstack/react-query';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import saveToekn from '../util/saveToekn';
 import { useUserStore } from '@/stores/userStore';
 
@@ -10,7 +11,6 @@ const useAuthMutation = () => {
     const { addToast } = useToastStore();
     const authRepository = new AuthRepositoryimpl();
     const router = useRouter();
-    const searchParams = useSearchParams();
     const { setUser } = useUserStore();
 
     const loginMutation = useMutation({
@@ -22,9 +22,11 @@ const useAuthMutation = () => {
             await saveToekn(accessToken, refreshToken);
             // 유저 정보 로컬 저장
             setUser({ email, nickName });
-            // 갈려하던 파라미터 저장
 
+            // 갈려하던 파라미터를 불러와 처리
+            const searchParams = new URLSearchParams(window.location.search);
             const callbackUrl = searchParams.get('callbackUrl') || '/';
+
             addToast(message, 'success');
             router.push(callbackUrl);
         }
