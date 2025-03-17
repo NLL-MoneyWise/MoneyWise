1f64e274313639a46d28ed57571f9ed217044b12
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-03-18 00:38:51 +0900

    feat : 캘린더 페이지 개발

---
+++ frontend/src/app/(diary)/calendar/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import Calendar from '@/app/diary/components/Calendar/Calendar';
+import React from 'react';
+
+const CalendarPage = () => {
+    return (
+        <div>
+            <Calendar />
+        </div>
+    );
+};
+
+export default CalendarPage;
