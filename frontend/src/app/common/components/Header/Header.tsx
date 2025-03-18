818131b1f1afeb8f800a8eda34f63bdc3be3778b
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-03-19 00:29:27 +0900

    캘린더 페이지 개발  (#50)
    * env : 캘린더 의존성 설치
    
    * design : 로그인 페이지 디자인 수정
    
    * design : 회원가입 디자인 수정
    
    * design : auth 레이아웃 디자인 수정
    
    * feat : 캘린더 페이지 개발
    
    * feat : 플로팅 버튼 컴포넌트 개발
    
    * design : 캘린더 css수정

--- frontend/src/app/common/components/Header/Header.tsx
+++ frontend/src/app/common/components/Header/Header.tsx
@@ -5,12 +5,12 @@ import Button from '../Button/Button';
 const Header = () => {
     return (
         <header className=" py-6 fixed left-0 right-0 top-0  bg-white shadow-md z-50">
-            <div className=" max-w-[1200px] m-auto">
+            <div className=" max-w-screen-xl m-auto">
                 <div className="items-center flex justify-center sm:justify-between w-full px-1 ">
                     <h1 aria-label="로고" className="">
                         <Logo variant="dark" width={250} height={40} />
                     </h1>
-                    <Button width={120} height={40} className="hidden sm:block">
+                    <Button className="hidden sm:block w-32 h-10 text-base">
                         시작하기
                     </Button>
                 </div>
