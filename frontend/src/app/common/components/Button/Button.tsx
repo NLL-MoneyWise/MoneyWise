55895e4937671731b63aa16058a83bd9717fa199
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-03-28 20:37:17 +0900

    design : 페이지 스타일 수정

--- frontend/src/app/common/components/Button/Button.tsx
+++ frontend/src/app/common/components/Button/Button.tsx
@@ -6,14 +6,27 @@ import { ReactNode } from 'react';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     handleClick?: () => void;
+    variant?: 'primary' | 'secondary';
 }
 
 // 차후에 수정
-const Button = ({ children, handleClick, className, ...rest }: ButtonProps) => {
+const Button = ({
+    variant = 'primary',
+    children,
+    handleClick,
+    className,
+    ...rest
+}: ButtonProps) => {
     return (
         <button
             className={clsx(
-                'bg-primary/90 flex justify-center items-center py-2 rounded-xl',
+                'flex justify-center items-center py-2 rounded-xl cursor-pointer',
+                {
+                    'bg-primary/80 text-white  hover:bg-primary':
+                        variant === 'primary',
+                    'bg-gray-200 text-gray-800  hover:bg-gray-300':
+                        variant === 'secondary'
+                },
                 className
             )}
             onClick={handleClick}
