7932fffe7898412f0066e2e5810af2b961d7ea80
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-04-19 02:02:42 +0900

    feat : 카카오 로그인 suspense 설정

--- frontend/src/app/(kakaologin)/kakao-login/page.tsx
+++ frontend/src/app/(kakaologin)/kakao-login/page.tsx
@@ -3,7 +3,7 @@ import useAuthMutation from '@/app/auth/hooks/useAuthMutation';
 import Text from '@/app/common/components/Text/Text';
 import Image from 'next/image';
 import { useRouter, useSearchParams } from 'next/navigation';
-import React, { useEffect } from 'react';
+import React, { Suspense, useEffect } from 'react';
 
 const KakaoLoginPage = () => {
     const router = useRouter();
@@ -22,15 +22,17 @@ const KakaoLoginPage = () => {
 
     return (
         <>
-            <Image
-                src={'/마스코트.png'}
-                alt="마스코트 이미지"
-                width={320}
-                height={320}
-            />
-            <Text.Title>로그인 중입니다.</Text.Title>
+            <Suspense fallback={<div>로딩 중...</div>}>
+                <Image
+                    src={'/마스코트.png'}
+                    alt="마스코트 이미지"
+                    width={320}
+                    height={320}
+                />
+                <Text.Title>로그인 중입니다.</Text.Title>
 
-            <Text.SubTitle>잠시만 기다려주세요</Text.SubTitle>
+                <Text.SubTitle>잠시만 기다려주세요</Text.SubTitle>
+            </Suspense>
         </>
     );
 };
