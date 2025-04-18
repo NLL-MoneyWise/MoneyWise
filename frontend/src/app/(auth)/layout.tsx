7265c63e1781e457d7ba3dfda2b88b5934fb0b0b
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-04-19 01:59:50 +0900

    chore :  파일 구조 변경

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -7,13 +7,26 @@ export default function AuthLayout({
     children: React.ReactNode;
 }) {
     return (
-        <>
+        <div
+            className="
+        min-w-[375px] 
+        max-w-[768px] 
+        w-full 
+        h-screen
+        min-h-screen
+        flex
+        bg-white
+        flex-col
+        mx-auto
+        box-border
+        "
+        >
             <TopBar />
 
             <div className="flex flex-col w-full h-full items-center justify-center">
                 <Logo variant="dark" width={400} height={41.7} />
                 <div className="w-full space-y-4 mt-10">{children}</div>
             </div>
-        </>
+        </div>
     );
 }
