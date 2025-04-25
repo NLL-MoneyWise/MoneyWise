f2ef53f964b7dfa14df433dd97cdc471e1578fae
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-04-26 02:26:36 +0900

    feat : 인풋 필드 컴포넌트 수정
    - 타입을 잘못 설정한 것을 발견

--- frontend/src/app/common/components/Input/InputField.tsx
+++ frontend/src/app/common/components/Input/InputField.tsx
@@ -7,6 +7,7 @@ interface BaseInputFieldProps {
     label?: string;
     type?: React.HTMLInputTypeAttribute;
     placeholder?: string;
+    className?: string;
 }
 
 // 제어 컴포넌트용 props
@@ -15,9 +16,17 @@ interface ControlledInputProps extends BaseInputFieldProps {
     onChange: (
         e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
     ) => void;
+    defaultValue?: never;
 }
 
-type InputFieldProps = ControlledInputProps | BaseInputFieldProps;
+// 비제어 컴포넌트용 props 추가
+interface UncontrolledInputProps extends BaseInputFieldProps {
+    defaultValue?: string;
+    value?: never;
+    onChange?: never;
+}
+
+type InputFieldProps = ControlledInputProps | UncontrolledInputProps;
 
 const commonInputStyles =
     'w-full px-3 py-2 border border-gray-300  rounded-xl shadow-sm focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary text-xl ';
@@ -25,40 +34,43 @@ const commonInputStyles =
 const InputField = forwardRef<
     HTMLInputElement | HTMLTextAreaElement,
     InputFieldProps
->(({ element = 'input', label, type, placeholder, ...rest }, ref) => {
-    return (
-        <div className="flex flex-col gap-2 font-medium">
-            {label && (
-                <label
-                    htmlFor={type}
-                    className={clsx(
-                        'text-lg font-medium text-primary relative'
-                    )}
-                >
-                    {label}
-                </label>
-            )}
-            {element === 'input' ? (
-                <input
-                    ref={ref as React.RefObject<HTMLInputElement>}
-                    id={type}
-                    type={type}
-                    placeholder={placeholder}
-                    {...rest}
-                    className={`${commonInputStyles} `}
-                />
-            ) : (
-                <textarea
-                    ref={ref as React.RefObject<HTMLTextAreaElement>}
-                    id={type}
-                    placeholder={placeholder}
-                    {...rest}
-                    className={`${commonInputStyles} min-h-[100px] resize-y`}
-                />
-            )}
-        </div>
-    );
-});
+>(
+    (
+        { element = 'input', label, type, placeholder, className, ...rest },
+        ref
+    ) => {
+        return (
+            <div className={clsx('flex flex-col gap-2 font-medium', className)}>
+                {label && (
+                    <label
+                        htmlFor={type}
+                        className={'text-lg font-medium text-primary relative'}
+                    >
+                        {label}
+                    </label>
+                )}
+                {element === 'input' ? (
+                    <input
+                        ref={ref as React.RefObject<HTMLInputElement>}
+                        id={type}
+                        type={type}
+                        placeholder={placeholder}
+                        {...rest}
+                        className={`${commonInputStyles} `}
+                    />
+                ) : (
+                    <textarea
+                        ref={ref as React.RefObject<HTMLTextAreaElement>}
+                        id={type}
+                        placeholder={placeholder}
+                        {...rest}
+                        className={`${commonInputStyles} min-h-[100px] resize-y`}
+                    />
+                )}
+            </div>
+        );
+    }
+);
 
 InputField.displayName = 'InputField';
 export default InputField;
