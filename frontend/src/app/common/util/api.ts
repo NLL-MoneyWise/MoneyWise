7483c00769491826b7db03b47a939e66be73a2b0
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-04-28 01:19:41 +0900

    feat : api 인터셉터 로직 추가
    - 리프레쉬로 데이터 받아오게 처리
    - 그에 맞춰 미들웨어 로직 제거

--- frontend/src/app/common/util/api.ts
+++ frontend/src/app/common/util/api.ts
@@ -1,7 +1,12 @@
+import { RefreshValidateResponse } from './../../auth/types/reponse/reponse-validate';
 import axios from 'axios';
 import type { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 import { CustomError } from '../types/error/error';
-import { getAccessToken } from '@/app/auth/util/toekn';
+import {
+    getAccessToken,
+    removeAccessToken,
+    saveAccessToken
+} from '@/app/auth/util/toekn';
 
 const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
@@ -22,8 +27,6 @@ export const defaultApi = (
             return config;
         },
         (error) => {
-            console.error('토큰 가져오기 실패:', error);
-
             return Promise.reject(error);
         }
     );
@@ -32,15 +35,54 @@ export const defaultApi = (
         (response) => {
             return response;
         },
-        (error) => {
-            const errorMessage =
-                error.response?.data?.message || '알 수 없는 에러입니다.';
-            const errorType = error.response.data?.typeName || 'UNKNOWN';
-            const errorStatus = error.response?.status || 500;
+        async (error) => {
+            const originalRequest = error.config;
 
-            return Promise.reject(
-                new CustomError(errorMessage, errorStatus, errorType)
-            );
+            if (error.response?.status === 401 && !originalRequest._retry) {
+                originalRequest._retry = true;
+
+                try {
+                    const response = await fetch(`${baseUrl}/auth/refresh`, {
+                        method: 'POST',
+                        credentials: 'include',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+
+                    const data: RefreshValidateResponse = await response.json();
+
+                    error.config.headers['Authorization'] =
+                        `Bearer ${data.access_token}`;
+
+                    await saveAccessToken(data.access_token);
+
+                    return instance(originalRequest);
+                } catch (refreshError) {
+                    const errorMessage =
+                        error.response?.data?.message ||
+                        '알 수 없는 에러입니다.';
+                    const errorType =
+                        error.response.data?.typeName || 'UNKNOWN';
+                    const errorStatus = error.response?.status || 500;
+
+                    window.location.href = `/login?callbackUrl=${window.location.pathname}&error=token_expired`;
+                    await removeAccessToken();
+
+                    return Promise.reject(
+                        new CustomError(errorMessage, errorStatus, errorType)
+                    );
+                }
+            } else {
+                const errorMessage =
+                    error.response?.data?.message || '알 수 없는 에러입니다.';
+                const errorType = error.response.data?.typeName || 'UNKNOWN';
+                const errorStatus = error.response?.status || 500;
+
+                return Promise.reject(
+                    new CustomError(errorMessage, errorStatus, errorType)
+                );
+            }
         }
     );
 
