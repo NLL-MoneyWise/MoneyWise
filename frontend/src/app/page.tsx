217f3295278f34e82d41aae6cec743cd5f5af973
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-05-05 03:53:06 +0900

    chore : 사용하지 않은 import 제거

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,16 @@
 'use client';
-import { useUserStore } from '@/stores/userStore';
 import ArrowIcon from './common/components/ArrowIcon/ArrowIcon';
 import InstructionArticle from './common/components/Articles/InstructionArticle/InstructionArticle';
 import IntroduceArticle from './common/components/Articles/IntroduceArticle/IntroduceArticle';
-import FloatingActionButton from './common/components/FloatingButton/FloatingButton';
 import Header from './common/components/Header/Header';
-import useClientMount from './common/hooks/useClinetMount';
 
 export default function Home() {
-    const { isLoggedIn } = useUserStore();
-    const { isMounted } = useClientMount();
-
-    if (!isMounted) return null;
-
     return (
         <div className="overflow-auto no-scrollbar ">
             <Header />
             <IntroduceArticle />
             <InstructionArticle />
             <ArrowIcon className="animate-bounce" color="#b0aeae" />
-            {isLoggedIn() && <FloatingActionButton />}
         </div>
     );
 }
