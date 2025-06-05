29ea4c52bedabeb79d2e21b6d8246c1aca98187a
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-06-05 17:55:59 +0900

    feat : 사용하지 않는 라우팅 제거

--- frontend/src/app/common/components/NavBar/NavBar.tsx
+++ frontend/src/app/common/components/NavBar/NavBar.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import React from 'react';
 const navLinks = [
     { href: '/upload', label: '업로드' },
-    { href: '/mypage', label: '개인 정보' },
     { href: '/calendar', label: '달력' },
     { href: '/analyze', label: '분석' }
 ];
@@ -12,7 +11,7 @@ const NavBar = () => {
     return (
         <>
             <nav className="hidden md:block flex-grow mx-16 font-medium ">
-                <ul className="flex justify-between">
+                <ul className="flex justify-between ">
                     {navLinks.map((link, index) => (
                         <li key={index}>
                             <Link
